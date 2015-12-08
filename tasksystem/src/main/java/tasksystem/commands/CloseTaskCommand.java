913eb7002e4317d03a642a5515185b9804959c11
timmygilissen <timmy.gilissen@gmail.com>
2015-12-08 22:17:03 +0100

    added closeTask

---
+++ tasksystem/src/main/java/tasksystem/commands/CloseTaskCommand.java
@@ -0,0 +1,39 @@
+package tasksystem.commands;
+
+
+public class CloseTaskCommand {
+    public final Long TaskId;
+
+    public final String Executor;
+
+    private CloseTaskCommand(Builder builder) {
+        TaskId = builder.TaskId;
+        Executor = builder.Executor;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private Long TaskId;
+        private String Executor;
+
+        private Builder() {
+        }
+
+        public Builder withTaskId(Long val) {
+            TaskId = val;
+            return this;
+        }
+
+        public Builder withExecutor(String val) {
+            Executor = val;
+            return this;
+        }
+
+        public CloseTaskCommand build() {
+            return new CloseTaskCommand(this);
+        }
+    }
+}
